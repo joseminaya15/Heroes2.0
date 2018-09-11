c53bb68abe6cb0afcbb4ff8c69c66902e517ed6d
joseminaya15 <jose.minayac15@gmail.com>
2018-09-11 10:01:49 -0500

    codeigniter

---
+++ application/controllers/Actividades.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Actividades extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('v_actividades');
+	}
+}
